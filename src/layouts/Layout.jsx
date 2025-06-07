9762d28959cbaf56e958960b7a72de5df6c80bbe
Reza <126569202+rezaghazikhani@users.noreply.github.com>
2025-06-07 19:23:55 +0330

    Layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,9 +1,14 @@
-function Layout({children}) {
+import styles from './Layout.module.css'
+
+function Layout({ children }) {
     return (
         <>
-            <header> heabet</header>
+            <header  className={styles.heaber}>
+                <h1>Book App </h1>
+                <p><a href="https://rezaghazikhani.ir">rgk.Code</a> | React.js Full Course</p>
+            </header>
             {children}
-            <footer> footer </footer>
+            <footer className={styles.footer}> <p>Developed by Reza with ❤</p> </footer>
         </>
     )
 }
