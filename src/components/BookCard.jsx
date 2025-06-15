a5b505e9f0a5d95876da3d858a121ae772db1678
Reza <126569202+rezaghazikhani@users.noreply.github.com>
2025-06-15 22:54:45 +0330

    like books

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,18 +1,31 @@
-function BookCard({data : {titel , author , image , language , pages}}) {
-  return (
-    <div>
-        <img src={image} alt={titel}/>
-        <div>
-            <h3>{titel}</h3>
-            <p>{author}</p>
-            <div>
-                <span>{language}</span>
-                <span>{pages}</span>
+import { useState } from "react"
+import { AiFillHeart } from "react-icons/ai"
+
+import styles from './BookCard.module.css'
+
+function BookCard({ data: { titel, author, image, language, pages } }) {
+
+    const [like, setLike] = useState(false);
+
+
+    const likeHandeler = () => {
+        setLike((like) => !like)
+    }
+
+    return (
+        <div className={styles.card}>
+            <img src={image} alt={titel} />
+            <div className={styles.info}>
+                <h3>{titel}</h3>
+                <p>{author}</p>
+                <div>
+                    <span>{language}</span>
+                    <span>{pages}</span>
+                </div>
             </div>
+            <button onClick={likeHandeler}><AiFillHeart color={like ? "red" : "#e0e0e0"} fontSize="1.8rem" /></button>
         </div>
-        <button>like</button>
-    </div>
-  )
+    )
 }
 
 export default BookCard
